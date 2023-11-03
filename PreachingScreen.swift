e036f1fa77416d65be73440af1184e0d9708ec3e
michaelyou1323 <73140783+michaelyou1323@users.noreply.github.com>
2023-11-03 13:00:42 +0200

    Design changes 2

--- PreachingScreen.swift
+++ PreachingScreen.swift
@@ -27,26 +27,58 @@ struct PreachingScreen: View {
                     .bold()
                 
             }  .frame( height: 60).padding(.horizontal, 80)
-                .padding(.vertical, 5)
+                .padding(.top, 30)
          
             
             ZStack {
-                
                 let shape = RoundedRectangle(cornerRadius: 20)
                 shape.fill().foregroundColor(.gray.opacity(0.01))
                 
                 shape.strokeBorder(lineWidth: 1)
-                    .frame( height: 190).padding(.horizontal, 20)
-                    .padding(.vertical, 15)
+                    .frame(height: 500)
+                    .padding(.horizontal, 1)
+                    .padding(.top, 1)
                 
-                Text("ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ ").font(.system(size:27 ))
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 40)
-                        .padding(.vertical, 5)
-                
-            }  .frame( height: 500).padding(.horizontal, 40)
-                .padding(.vertical, 5)
+                VStack(alignment: .center) {
+                    Text("ما يُنشئهُ الإِيمَانُ")
+                        .font(.system(size: 30))
+                        .bold()
+                        .multilineTextAlignment(.center) // Center alignment
+                        .padding(.horizontal, 40)
+                        
+                        .foregroundStyle(.red)
+                    
+                    
+                    HStack {
+                        Spacer() // Spacer to push the next Text to the right
+                        Text("وَأَمَّا الإِيمَانُ فَهُوَ الثِّقَةُ بِمَا يُرْجَى وَالإِيقَانُ بِأُمُورٍ لاَ تُرَى ( عبرانيين 11: 1 )")
+                            .font(.system(size: 22))
+                            .bold()
+                            .multilineTextAlignment(.trailing)
+                           
+                            .padding(.vertical, 10)
+                            .padding(.trailing, 10)
+
+                            .foregroundStyle(.blue)
+                    }
+                    
+                    HStack {
+                        Spacer() // Spacer to push the next Text to the right
+                        Text("في أصحاح الإيمان الشهير في الرسالة إلى العبرانيين، أوضح الرسول مجالين لعمل الإيمان: الأول هو الثِّقَةُ بِمَا يُرْجَى، والثاني هو الإِيقَانُ بِأُمُورٍ لاَ تُرَى ( عب 11: 1 ). هذا معناه أن الإيمان يعمل في دائرتين: غير الحادث، أو المستقبل، وغير المنظور، أو الروحي. الإيمان يُمكِّن الشخص المؤمن من أن يرى المستقبل كما يرى الحاضر، والذي لا يُرى كأنه يُرى. بعبارة أخرى، إن الإيمان هو تليسكوب يقرِّب البعيد، وهو أشعة إكس يكشف بواطن الأمور.")
+                            .font(.system(size: 20))
+                            .bold()
+                            .multilineTextAlignment(.trailing)
+                            .padding(.trailing, 10)
+                            
+                            .foregroundStyle(.black)
+                            .padding(.bottom, 30)
+                    }
+                }
+            }
+            .frame(height: 500)
+            .padding(.horizontal, 25)
+            .padding(.vertical, 99)
+            
          
             
             
